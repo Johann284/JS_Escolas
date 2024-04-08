a280882dd8aff3530633899471c54372b566ca32
Johann284 <160616489+Johann284@users.noreply.github.com>
2024-04-08 12:47:52 -0300

    o sete ta erradu

--- js/primeiraListaProgramacao/sete.js
+++ js/primeiraListaProgramacao/sete.js
@@ -14,13 +14,18 @@ i. Hawaiano custa R$:5,00
 let sabores = ['Chocolate', 'Morango', 'Creme', 'Manga', 'Melancia', 'Vanilla', 'Céu Azul', 'Brownie', 'Hawaiano'];
 let valores = [1.50, 2.50, 2.5, 3.20, 3.40, 3.00, 3.60, 4.00, 5.00];
 let i = 0;
-var igual = false;
+var igual = 0;
 while (i !== 9){
     console.log(sabores[i]);
     i = i + 1;
 }
 
 
-let sabor = prompt("Escolha um sabor! ");
+let sabor = prompt("Escolha um sabor digitando seu nome: ");
 
-
+while (igual > 9){
+    if(sabor == sabores[i]){
+        console.log(valores[i]);
+        igual += 1;
+    }
+}
